13707712e0a693da7f1faac1d3103399a8b271d5
Davi Camarinha <davi.camarinha@thoughtworks.com>
2026-03-12 14:20:38 -0300

    Revert "Primeiro commit"
    This reverts commit cd0d5a0044921283ceb83e3e9bea7329a7fb17a6.

--- prisma/migrations/20260130192658_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "SentDeal" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "store" TEXT NOT NULL,
-    "price" REAL NOT NULL,
-    "sentAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
